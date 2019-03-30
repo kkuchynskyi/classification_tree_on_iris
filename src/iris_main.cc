51346afeb5d94a16110b496181c29bcb17604a34
kyryll <kuchynskii1999@gmail.com>
2019-03-30 18:06:41 +0200

    "delete files"

--- src/iris_main.cc
+++
@@ -1,9 +0,0 @@
-// #include "data_handler.hpp"
-// #include "data_handler.cc"
-
-// int main()
-// {   
-//     data_handler *iris = new data_handler(151,"iris/iris.csv");
-
-//     printf("HELLO.\n");
-// }
